0f6c94b562eeee9afa8308da438bab732cc290ee
Palaz <zoltan.palatinus@inteliart.hu>
2019-03-23 16:31:19 +0100

    handling errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Axios from 'axios';
 
 const INGREDIENT_PRICES = {
     salad: 0.3,
@@ -96,7 +98,7 @@ class BurgerBuilder extends Component {
          },
          deliveryMethod: 'fastest'
       }
-      axios.post('/orders.json', order)
+      axios.post('/orders', order)
       .then(response => {
          this.setState({loading: false, purchasing: false});
       })
@@ -138,5 +140,5 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
 
